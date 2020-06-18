14ff9d42c1d68da5968b3d159f47e4756cdd0472
Kélian Raoult <naylekelian>
2020-06-18 10:23:18 +0200

    ADD link between HTML and CSS page

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,8 @@ module.exports = {
       patterns: [{
         from: path.resolve(__dirname, 'src', 'index.html'),
         to: path.resolve(__dirname, 'dist'),
+        from: path.resolve(__dirname, 'src', 'css/style.css'),
+        to: path.resolve(__dirname, 'dist/css'),
       }, ],
     }),
   ],
