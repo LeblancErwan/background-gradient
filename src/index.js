b504ad00700db8f559fe2a09a6152856ac425535
Kélian Raoult <naylekelian>
2020-06-30 17:07:29 +0200

    ADD - modify CSS properties with ranges for sections 2 and 5 (JS)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // section 1 linear gradient
-function linearGradientFirstExample() {
+const linearGradientFirstExample = function () {
     let backgroundLinearGradientFirstExample = document.getElementById("visualForLinearGradientFirstExample");
     const degrees = document.querySelector('input[name="degrees"]')
     const width = document.querySelector('input[name="width"]')
@@ -7,13 +7,33 @@ function linearGradientFirstExample() {
     backgroundLinearGradientFirstExample.style.background = "linear-gradient(" + degrees.value + "deg, rgb(0, 204, 203) " + width.value + "%, transparent " + degraded.value + "%), url('../images/linearGradient1.jpg')";
 }
 
-let inputsLinearGradientFirstExample = document.querySelectorAll(".linearGradientFirstExample input");
-for (let i = 0; i < inputsLinearGradientFirstExample.length; i++) {
-    inputsLinearGradientFirstExample[i].addEventListener("input", linearGradientFirstExample)
+// section 2 linear gradient
+const linearGradientSecondExample = function () {
+    let backgroundLinearGradientSecondExample = document.getElementById("visualForLinearGradientSecondExample");
+    const colorsNumber = document.querySelector('input[name="colorsNumber"]')
+    const colorsArray = [
+        "linear-gradient(30deg, rgb(121, 248, 248) 100%, transparent 43%)",
+        "linear-gradient(135deg, rgb(223, 115, 255) 55%, transparent 45%)",
+        "linear-gradient(45deg, rgb(253, 108, 158) 45%, transparent 45%)",
+        "linear-gradient(-45deg, rgb(96, 80, 220) 30%, transparent 30%)"
+    ]
+    let cssProperty = "";
+
+    for (let i = 0; i < colorsNumber.value; i++) {
+        if (i == colorsNumber.value - 1) {
+            cssProperty = colorsArray[i] + cssProperty;
+
+        } else {
+            cssProperty = ", " + colorsArray[i] + cssProperty;
+
+        }
+        backgroundLinearGradientSecondExample.style.background = cssProperty;
+
+    }
 }
 
-// section 2 linear gradient
-function linearGradientSecondExample() {
+// section 3 linear gradient
+const linearGradientThirdExample = function () {
     let backgroundLinearGradientThirdExample = document.getElementById("visualForLinearGradientThirdExample");
     const firstDegraded = document.querySelector('input[name="firstDegraded"]')
     const secondDegraded = document.querySelector('input[name="secondDegraded"]')
@@ -24,7 +44,37 @@ function linearGradientSecondExample() {
     console.log("linear-gradient(-47 deg, rgb(248, 142, 85) 25%, transparent" + firstDegraded.value + "%), linear-gradient(45 deg, rgb(253, 108, 158) 34%, transparent" + secondDegraded.value + "%), linear-gradient(100 deg, rgb(223, 115, 255) 55%, transparent" + thirdDegraded.value + "%), linear-gradient(30 deg, rgb(121, 248, 248) 100%, transparent" + fourthDegraded.value + "%)")
 }
 
-let inputsLinearGradientThirdExample = document.querySelectorAll(".linearGradientThirdExample input");
-for (let i = 0; i < inputsLinearGradientThirdExample.length; i++) {
-    inputsLinearGradientThirdExample[i].addEventListener("input", linearGradientSecondExample)
+// section 2 radial gradient
+const radialGradientSecondExample = function () {
+    let backgroundRadialGradientSecondExample = document.getElementById("visualForRadialGradientSecondExample");
+    const degradeSecondRadialGradient = document.querySelector('input[name="degradeSecondRadialGradient"]')
+    const XplaceSecondRadialGradient = document.querySelector('input[name="XplaceSecondRadialGradient"]')
+    const YplaceSecondRadialGradient = document.querySelector('input[name="YplaceSecondRadialGradient"]')
+    const circleAndEllipseArray = [
+        circleAndEllipse = document.querySelector('.radialGradientSecondExample input[name="circleAndEllipseChecked"]')
+    ]
+
+    for (let i = 0; i < circleAndEllipseArray.length; i++) {
+
+        if (circleAndEllipseArray[0].checked == true) {
+            shape = "circle "
+        } else {
+            shape = "ellipse "
+        }
+        backgroundRadialGradientSecondExample.style.background = "radial-gradient(" +
+            shape + "at " + XplaceSecondRadialGradient.value + "% " + YplaceSecondRadialGradient.value + "%, transparent 30%, rgb(96, 80, 220) " + degradeSecondRadialGradient.value + "%)";
+    }
+
 }
+
+const addEventListenerToInputs = function (inputsCalled, functionCalled) {
+    let inputsArray = document.querySelectorAll(inputsCalled);
+    for (let i = 0; i < inputsArray.length; i++) {
+        inputsArray[i].addEventListener("input", functionCalled)
+    }
+}
+
+addEventListenerToInputs(".linearGradientFirstExample input", linearGradientFirstExample)
+addEventListenerToInputs(".linearGradientThirdExample input", linearGradientThirdExample)
+addEventListenerToInputs(".linearGradientSecondExample input", linearGradientSecondExample)
+addEventListenerToInputs(".radialGradientSecondExample input", radialGradientSecondExample)
