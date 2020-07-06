600cbc613f18503479bb4d551b21b9f09e32a285
Erwan Leblanc <freelance@eleblanc.fr>
2020-07-06 12:20:07 +0200

    ADD - github pages build

--- webpack.config.js
+++ webpack.config.js
@@ -13,15 +13,15 @@ module.exports = {
       patterns: [
         {
           from: path.resolve(__dirname, 'src', 'index.html'),
-          to: path.resolve(__dirname, 'dist'),
+          to: path.resolve(__dirname, 'docs'),
         },
         {
           from: path.resolve(__dirname, 'src/css'),
-          to: path.resolve(__dirname, 'dist/css'),
+          to: path.resolve(__dirname, 'docs/css'),
         },
         {
           from: path.resolve(__dirname, 'src/images'),
-          to: path.resolve(__dirname, 'dist/images'),
+          to: path.resolve(__dirname, 'docs/images'),
         },
       ],
       options: {
